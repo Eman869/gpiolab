99d9fb1c3712cce0e80c54912e59707d81197f2c
Bossen WU <bossen.wu@st.com>
2022-02-24 15:16:01 +0800

    remove unnecessary header files

--- src/gpiolab.c
+++ src/gpiolab.c
@@ -3,18 +3,14 @@
 /*
  * gpiolab.c
  *
- *  Created on: Jan 25, 2022
- *      Author: bossenwu
+ *  Created on: Feb 24, 2022
+ *      Author: Bossen Wu
  */
 
 #include <errno.h>
 #include <gpiod.h>
-#include <getopt.h>
-#include <limits.h>
-#include <poll.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/select.h>
 #include <unistd.h>
 
 #define GPIOA   "/dev/gpiochip0"
@@ -22,43 +18,43 @@
 
 int main(void)
 {
-    struct gpiod_line *line;
+	struct gpiod_line *line;
 	struct gpiod_chip *gpiochip;
-    unsigned int count = 0;
-    int i, value = 0, ret = 0;
-    
-    gpiochip = gpiod_chip_open(GPIOA);
-    line = gpiod_chip_get_line(gpiochip, PA14);
-    if (line == NULL)
-    	goto EXIT_CHIP;
+	unsigned int count = 0;
+	int i, value = 0, ret = 0;
 
-    ret = gpiod_line_request_output(line, "gpiolab", 0);
-    if (ret)
-    {
-    	printf("gpiod_line_request_output failed \n");
-    	goto EXIT_LINE;
-    }
+	gpiochip = gpiod_chip_open(GPIOA);
+	line = gpiod_chip_get_line(gpiochip, PA14);
+	if (line == NULL)
+		goto EXIT_CHIP;
 
-    do {
-    	count++;
-    	for (i=0; i < 6; i++)
-    	{
-    		value = (value) ? 0 : 1;
-    		ret = gpiod_line_set_value(line, value);
-    		if (ret)
-    			goto EXIT_LINE;
+	ret = gpiod_line_request_output(line, "gpiolab", 0);
+	if (ret)
+	{
+		printf("gpiod_line_request_output failed \n");
+		goto EXIT_LINE;
+	}
 
-    		usleep(200000);
-    	}
-    	sleep(1);
-    } while (count < 5);
+	do {
+		count++;
+		for (i=0; i < 6; i++)
+		{
+			value = (value) ? 0 : 1;
+			ret = gpiod_line_set_value(line, value);
+			if (ret)
+				goto EXIT_LINE;
 
-EXIT_LINE:
-    gpiod_line_release(line);
+			usleep(200000);
+		}
+		sleep(1);
+	} while (count < 5);
 
-EXIT_CHIP:
+	EXIT_LINE:
+	gpiod_line_release(line);
+
+	EXIT_CHIP:
 	gpiod_chip_close(gpiochip);
 
 	printf("gpiolab is completed \n");
-    return 0;
+	return 0;
 }
